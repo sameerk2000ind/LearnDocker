2d667a3c5566cb87e1f0b2d332453d9c425e0a07
sameerkumarptx <sameerkumarptx@users.noreply.github.com>
2020-01-13 03:30:31 -0500

    Test

--- WeatherForecast.cs
+++ WeatherForecast.cs
@@ -11,7 +11,5 @@ namespace LearnDocker
         public int TemperatureF => 32 + (int)(TemperatureC / 0.5556);
 
         public string Summary { get; set; }
-
-        public string NewSummary { get; set; }
     }
 }
